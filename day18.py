1523429a51942f1bb7a9cdbc5e2a4533d138232b
Merwin-Rebello <110761387+Merwin-Rebello@users.noreply.github.com>
2023-06-13 16:53:32 +0530

    Add files via upload

---
+++ day18.py
@@ -0,0 +1,12 @@
+#Best Time to Buy and Sell Stock
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit =0
+        low = float("inf")
+        for i in prices:
+            if low>i:
+                low=i
+            elif i -low>profit:
+                profit = i-low
+        return profit            
