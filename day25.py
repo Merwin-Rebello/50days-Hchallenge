774730c70d646300d51087c20b775b5337354278
Merwin-Rebello <110761387+Merwin-Rebello@users.noreply.github.com>
2023-06-23 21:49:17 +0530

    Add files via upload

---
+++ day25.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+  def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        def solving(root1,root2):
+            if not root1 and not root2:
+                return True
+            if not root1 and root2:
+                return False
+            if root1 and not root2:
+                return False
+            if root1.val != root2.val:
+                return False
+            return solving(root1.left,root2.left) and solving(root1.right,root2.right)
+        return solving(p,q)
