cc979472b7dd7abf4f0d93e459ca76ed4270805b
Merwin-Rebello <110761387+Merwin-Rebello@users.noreply.github.com>
2023-07-04 22:32:49 +0530

    Add files via upload

---
+++ day33.py
@@ -0,0 +1,10 @@
+#Single Number II
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        count = defaultdict(int)
+        for i in nums:
+            count[i] += 1
+        for k,v in count.items():
+            if v == 1:
+                return k
+        return -1 
