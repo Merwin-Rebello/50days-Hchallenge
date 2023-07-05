f263add223e5e564ba5bc98ed31eb2f1835d2301
Merwin-Rebello <110761387+Merwin-Rebello@users.noreply.github.com>
2023-07-05 22:20:49 +0530

    Add files via upload

---
+++ day34.py
@@ -0,0 +1,10 @@
+# Search in Rotated Sorted Array
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        for i in range(len(nums)):
+            if nums[i]==target:
+                return nums.index(nums[i])
+                break
+        else:
+            return -1      
